4336473e2db7d1f11ed3e7c91267eaceef204916
adriton <adrtler@gmail.com>
2021-05-09 11:42:25 +0200

    Pequeña paginación

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,15 +1,28 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { reqResApi } from '../api/reqRes';
 import { ReqResListado, Usuario } from '../interfaces/reqRes';
 export const Usuarios = () => {
   const [usuarios, setUsuarios] = useState<Usuario[]>([]);
   useEffect(() => {
     // Llamado API
-    reqResApi
-      .get<ReqResListado>('/users')
-      .then((res) => setUsuarios(res.data.data))
-      .catch(console.log);
+    cargarUsuarios();
   }, []);
+
+  const pageRef = useRef(1);
+
+  const cargarUsuarios = async () => {
+    const {
+      data: { data },
+    } = await reqResApi.get<ReqResListado>('/users', {
+      params: { page: pageRef.current },
+    });
+    if (data.length > 0) {
+      setUsuarios(data);
+      pageRef.current++;
+    } else {
+      alert('No hay más registros');
+    }
+  };
 
   const renderItem = ({
     id,
@@ -47,6 +60,9 @@ export const Usuarios = () => {
         </thead>
         <tbody>{usuarios.map((usuario) => renderItem(usuario))}</tbody>
       </table>
+      <button className="btn btn-primary" onClick={cargarUsuarios}>
+        Siguientes
+      </button>
     </>
   );
 };
